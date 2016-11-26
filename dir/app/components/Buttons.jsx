befcc1a4f815f82fa9a62e785c5d574317c134f8
arsenlosenko <arsenlosenko@gmail.com>
2016-11-26 12:21:23 +0200

    Responsive buttons

--- dir/app/components/Buttons.jsx
+++ dir/app/components/Buttons.jsx
@@ -4,8 +4,8 @@ export default class Test extends React.Component{
 	render(){
 		return(
 			<div>
-				<button id="left">&#8592;</button>
-				<button id="right">&#8594;</button>
+				<button type="button"  id="left" className="btn-lg">&#8592;</button>
+				<button type="button"  id="right" className="btn-lg">&#8594;</button>
 			</div>
 			);
 				}
