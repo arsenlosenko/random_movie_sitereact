00f47b92866c1634c50fa55386c4954452ea0821
arsenlosenko <arsenlosenko@gmail.com>
2016-12-05 23:10:05 +0200

    Optimization of App.jsx

--- dir/app/components/App.jsx
+++ dir/app/components/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import Footer from 'components/Footer';
-import Buttons from 'components/Buttons';
-//import Script from 'components/Script';
-import * as movies from 'components/movies';
-
-
+import Script from 'components/Script';
 
 export default class App extends React.Component {
 	constructor(){
 		super();
-	}
 
+}
   render() {
     return (
     	<div>
@@ -24,14 +19,88 @@ export default class App extends React.Component {
         <option value="action">Action</option>
         <option value="drama">Drama</option>
         <option value="history">History</option>
-        
       </select>
       <input type="submit" value="Random film" className="btn-primary" />
       </form>
+      <Script />
       <Buttons />
+      <div>{movieShow()}</div>
       <Footer />
       </div>
       </div>
     );
   }
+}
+
+
+class Buttons extends React.Component{
+	render(){
+		return(
+			<div>
+				<button type="button"  id="left" className="btn-lg">&#8592;</button>;
+				<button type="button"  id="right" className="btn-lg">&#8594;</button>;
+			</div>
+			);
+				}
+};
+
+
+class Footer extends React.Component{
+	render(){
+		return(
+			<h4 id="footer">Footer</h4>	
+			);
+
+	}
+}
+
+const movies = {
+  id_1: {
+    'genre': 'drama',
+    'title': 'Shawshank Redemprion',
+    'year': '1994',
+    'budget': '2.5M',
+    'img': 'https://goo.gl\/Dpyuth',
+  },
+  id_2: {
+    'genre': 'crime',
+    'title': 'The Godfather',
+    'year': '1972',
+    'budget': '1.5M',
+    'img': 'https://goo.gl\/Xulesy',
+  },
+  id_3: {
+    'genre': 'action',
+    'title': 'The Dark Knight',
+    'year': '2008',
+    'budget': '200M',
+    'img': 'https://goo.gl\/p6O2pv',
+  },
+  id: '',
+  genre: '',
+  title: '',
+  year: '',
+  budget: '',
+  img: '',
+
+  id: '',
+  genre: '',
+  title: '',
+  year: '',
+  budget: '',
+  img: '',
+
+}
+function movieShow(){
+
+	return(
+	<div id='movie'>
+		<h1>{movies.id_1.title}</h1>
+		<p>Genre: {movies.id_1.genre}</p>
+		<p>Year: {movies.id_1.year}</p>
+		<p>Budget: {movies.id_1.budget}</p>
+		<img src={movies.id_1.img}/>
+	</div>
+	);
+
 }
--- dir/app/components/movies.js
+++ dir/app/components/movies.js
@@ -1,7 +1,38 @@
-var movies;
-export default movies = {
-'id':'1',
-'name': 'Test',
-'description':'Test message',
-'image':'/image/shaw.jpg',
-};
+
+const movies = {
+  id_1: {
+    'genre': 'drama',
+    'title': 'Shawshank Redemprion',
+    'year': '1994',
+    'budget': '2.5M',
+    'img': 'https://goo.gl\/Dpyuth',
+  },
+  id_2: {
+    'genre': 'crime',
+    'title': 'The Godfather',
+    'year': '1972',
+    'budget': '1.5M',
+    'img': 'https://goo.gl\/Xulesy',
+  },
+  id_3: {
+    'genre': 'action',
+    'title': 'The Dark Knight',
+    'year': '2008',
+    'budget': '200M',
+    'img': 'https://goo.gl\/p6O2pv',
+  },
+  id: '',
+  genre: '',
+  title: '',
+  year: '',
+  budget: '',
+  img: '',
+
+  id: '',
+  genre: '',
+  title: '',
+  year: '',
+  budget: '',
+  img: '',
+
+}
